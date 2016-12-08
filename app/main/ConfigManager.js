56e51deba34abb1db193f1e433bb3b5d27a37d2f
木休大人 <523317421@qq.com>
2016-12-08 17:34:06 +0800

    修改配置文件引用路径

--- app/main/ConfigManager.js
+++ app/main/ConfigManager.js
@@ -5,16 +5,13 @@
  * @author mudio(job.mudio@gmail.com)
  */
 
-/* eslint no-underscore-dangle: [2, { "allowAfterThis": true }] */
-
-import os from 'os';
 import path from 'path';
 import ConfigStore from 'configstore';
 import {REGION_BJ, REGION_GZ} from '../bce/utils/Region';
 
-// TODO: 这里会生成~/.config/undefine.json
-const config = new ConfigStore();
-config.path = path.join(os.homedir(), '.bce', 'config.json');
+const bcedir = path.join('..', '.bce');
+const config = new ConfigStore(bcedir, {}, {globalConfigPath: true});
+
 config.all = Object.assign(
     {
         endpoint: {
